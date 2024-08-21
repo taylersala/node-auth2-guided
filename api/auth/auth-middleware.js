aa6fedbdb813ff2e2c13a0dac240186adf8963d7
taylersala <taylersala@gmail.com>
2024-08-20 23:50:35 -0700

    done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,14 +1,35 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../../config')
+
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization
+  if(token) {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        next({status: 401, message: `token bad ${err.message}`})
+      } else {
+        req.decodedJwt = decoded
+        next()
+      }
+    })
+  } else {
+    next({ status: 401, message: 'what no token?'})
+  }
 }
 
 // AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
+const checkRole = role => (req, res, next) => {
+  if ( req.decodedJwt && req.decodedJwt.role === role) {
+    next()
+  }else{
+    next({ status: 422, messgae: 'you have no power here'})
+  }
+  
 }
 
 module.exports = {
   restricted,
   checkRole,
 }
+
